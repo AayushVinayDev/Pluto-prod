74b30f0eacfd6deeb7523b318c45f911e5d7baea
Aayush Vinay <btech10104.19@bitmesra.ac.in>
2024-10-18 21:36:30 +0530

    added cognito

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -5,6 +5,7 @@ import React, { useEffect } from "react";
 import StoreProvider, { useAppSelector } from "./redux";
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
+import AuthProvider from "./authProvider";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isSidebarCollapsed = useAppSelector(
@@ -40,7 +41,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <DashboardLayout>{children}</DashboardLayout>
+      <AuthProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
   );
 };
