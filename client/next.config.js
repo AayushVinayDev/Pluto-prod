21276458d739df9b073ef29070be9e97e1996a9d
Aayush Vinay <btech10104.19@bitmesra.ac.in>
2024-10-18 21:08:49 +0530

    redirect images to s3

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pm-av-s3-images.s3.ap-south-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
+};
+
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+//https://pm-av-s3-images.s3.ap-south-1.amazonaws.com/i1.jpg
 
 module.exports = withBundleAnalyzer(nextConfig);
--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -173,7 +173,7 @@ const Task = ({ task }: TaskProps) => {
     >
       {task.attachments && task.attachments.length > 0 && (
         <Image
-          src={`/images/${task.attachments[0].fileURL}`}
+          src={`https://pm-av-s3-images.s3.ap-south-1.amazonaws.com/${task.attachments[0].fileURL}`}
           alt={task.attachments[0].fileName}
           width={400}
           height={200}
@@ -225,7 +225,7 @@ const Task = ({ task }: TaskProps) => {
             {task.assignee && (
               <Image
                 key={task.assignee.userId}
-                src={`/images/${task.assignee.profilePictureUrl!}`}
+                src={`https://pm-av-s3-images.s3.ap-south-1.amazonaws.com/${task.assignee.profilePictureUrl!}`}
                 alt={task.assignee.username}
                 width={30}
                 height={30}
@@ -235,7 +235,7 @@ const Task = ({ task }: TaskProps) => {
             {task.author && (
               <Image
                 key={task.author.userId}
-                src={`/images/${task.author.profilePictureUrl!}`}
+                src={`https://pm-av-s3-images.s3.ap-south-1.amazonaws.com/${task.author.profilePictureUrl!}`}
                 alt={task.author.username}
                 width={30}
                 height={30}
