9d48cefc0173b832c585020c44cddd1c5b9aea37
Aayush Vinay <btech10104.19@bitmesra.ac.in>
2024-10-18 19:32:33 +0530

    fixed build error

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,28 +1,28 @@
 "use client";
+
 import React, { useState } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader";
-import Board from "@/app/projects/BoardView";
-import List from "@/app/projects/ListView";
-import Timeline from "@/app/projects/TimelineView";
-import Table from "@/app/projects/TableView";
+import Board from "../BoardView";
+import List from "../ListView";
+import Timeline from "../TimelineView";
+import Table from "../TableView";
 import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
-  params: {
-    id: string;
-  };
+  params: { id: string };
 };
 
-const page = ({ params }: Props) => {
+const Project = ({ params }: Props) => {
   const { id } = params;
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
   return (
     <div>
-      {/* MODAL NEW TASK */}
-      <ModalNewTask 
-      isOpen={isModalNewTaskOpen}
-      onClose={() => setIsModalNewTaskOpen(false)}
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
       />
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
@@ -41,4 +41,4 @@ const page = ({ params }: Props) => {
   );
 };
 
-export default page;
+export default Project;
